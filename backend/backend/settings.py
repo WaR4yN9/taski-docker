7cdfc85d60b295b48337ea79d8e955d0a4305428
Kirill Cool <coolkv09@gmail.com>
2026-03-11 00:54:42 +0300

    docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,6 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 '130.193.55.206',
                  'coolfmyp.hopto.org',
                  '127.0.0.1',]
 
